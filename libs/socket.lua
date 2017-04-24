fd932709bdca7053b10e002d610a71e4e907912b
Katherine Robin <siapran.candoris@gmail.com>
2017-04-24 18:14:47 +0200

    Added socket.lua.

---
+++ libs/socket.lua
@@ -0,0 +1,23 @@
+
+local clients = {}
+
+local function client_connection (req, read, write)
+	-- Log the request headers
+	p("New client", req.socket)
+	
+	-- Log and echo all messages
+	for message in read do
+		p(message)
+		write(message)
+	end
+	-- End the stream
+	write()
+end
+
+local package = {
+	client_connection = client_connection
+}
+return package
+
+
+
